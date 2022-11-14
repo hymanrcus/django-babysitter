4f321426c8c4edd04e5b6ab40771d2db0c1304dc
Marcus <hymanmarcus95@gmail.com>
2022-11-13 19:30:27 -0500

    display babies

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-# localhost:8000/
-### NOT DOING THIS: localhost:8000/cats
+
+
 
 urlpatterns = [
   # localhost:8000/
   path('', views.home, name='home'),
-  path('about/', views.about ,name='about')
+  path('about/', views.about ,name='about'),
+  path('babies/', views.babies_index, name='babies_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+class Baby:
+  def __init__(self, name, description, age):
+    self.name = name
+    self.description = description
+    self.age = age
+
+babies = [
+  Baby('Lolo', 'Kinda rude.', 3),
+  Baby('Sachi', 'Looks like a turtle.', 0),
+  Baby('Fancy', 'Happy fluff ball.', 4),
+  Baby('Bonk', 'Meows loudly.', 6)
+]
+    
+
 # Create your views here.
 def home(request):
   return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+def babies_index(request):
+  return render(request, 'babies/index.html', { 'babies': babies })
