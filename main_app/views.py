77cb8d63847d879490234f495b9d4457655e7fb9
Marcus <hymanmarcus95@gmail.com>
2022-11-13 21:41:07 -0500

    add admin functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,27 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Baby
 
 
-class Baby:
-  def __init__(self, name, description, age):
-    self.name = name
-    self.description = description
-    self.age = age
+# class Baby:
+#   def __init__(self, name, description, age):
+#     self.name = name
+#     self.description = description
+#     self.age = age
 
-babies = [
-  Baby('Lolo', 'Kinda rude.', 3),
-  Baby('Sachi', 'Looks like a turtle.', 0),
-  Baby('Fancy', 'Happy fluff ball.', 4),
-  Baby('Bonk', 'Meows loudly.', 6)
-]
+# babies = [
+#   Baby('Lolo', 'Kinda rude.', 3),
+#   Baby('Sachi', 'Looks like a turtle.', 0),
+#   Baby('Fancy', 'Happy fluff ball.', 4),
+#   Baby('Bonk', 'Meows loudly.', 6)
+# ]
     
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def babies_index(request):
+  babies = Baby.objects.all()
   return render(request, 'babies/index.html', { 'babies': babies })
